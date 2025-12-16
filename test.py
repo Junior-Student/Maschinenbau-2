1d92e14a300174f81edb7cf3d19617e028e34195
Junior-Student <ein-qauantum-kater@gmx.de>
2025-12-16 13:21:29 +0000

    hi

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-print("hallo menschen") 
+Stahlstab = 150 
+Kraft = 1500
+Dehnung = 0.25
+Ergebnis = (Dehnung / Stahlstab * 100 *100)
+ergebnis = round(Ergebnis) 
+ergebniss = ergebnis
+print(ergebni
